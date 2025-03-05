b7c85c0c84fc2c0f6a65ccc81377ec992a1d4001
Rohan Rawat <rahonrawat194@gmail.com>
2025-03-05 13:36:54 +0530

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("Hello Guys");
+    res.send("Hello World");
 });
 
-app.listen(5000,()=>{
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
     console.log("Server Started!");
 })
