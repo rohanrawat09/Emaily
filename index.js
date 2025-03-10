4c9c3d260eaec7374e6e3cb2a4d1e237667f6441
Rohan Rawat <rahonrawat194@gmail.com>
2025-03-10 11:34:29 +0530

    Authentication complete

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
+const passport = require('passport');
+require('./models/Users.js');
+require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 24*30*60*60*1000, // 30 days
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoute.js')(app);
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
 });
+
 
 const PORT = process.env.PORT || 5000;
 
